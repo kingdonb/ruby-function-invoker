6bdd0829e0179f4959043d986d877ef884a6f4e5
Kingdon Barrett <kingdon.b@nd.edu>
2018-07-22 10:28:09 -0400

    correct CRD definitions and use updated image
    clean up samples and add deploy.sh to my sample

--- samples/base64/deploy.sh
+++ samples/base64/deploy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-docker build . -t yebyen/base64:0.0.3-snapshot
-docker push yebyen/base64:0.0.3-snapshot
-kubectl apply -f base64.yml
+riff create ruby
--- samples/binary/deploy.sh
+++ samples/binary/deploy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-docker build . -t yebyen/binary:0.0.3-snapshot
-docker push yebyen/binary:0.0.3-snapshot
-kubectl apply -f binary.yml
+riff create ruby
